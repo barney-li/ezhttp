cca05c0442df36816fe75e1f31ae3e52c38a8757
barney <barney.li@outlook.com>
2017-05-06 21:34:16 -0700

    trying to make getAsync work, yes, trying

--- ezhttp.lua
+++ ezhttp.lua
@@ -1,9 +1,12 @@
 local _M = {}
 local neturl = require("neturl")
-require("fun") ()
 local json = require("dkjson")
+local log = require("log"):new("debug")
+require("fun") ()
+
 _M.new = function(self)
 	local t = {}
+	t.uv = require("luv")
 	setmetatable(t.__index,_M)
 	return t
 end
@@ -23,10 +26,9 @@ end
 
 _M.get = function(url)
 	local u = neturl.parse(url)
-	print(u.host)
-	print(u.scheme)
-	print(u.port or u.services[u.scheme])
-	io.flush()
+	log:debug(u.host)
+	log:debug(u.scheme)
+	log:debug(u.port or u.services[u.scheme])
 	local uv = require("luv")
 	local client = uv.new_tcp()
 	local ret_err, ret_res
@@ -39,47 +41,80 @@ _M.get = function(url)
 		ret_err = err
 		uv.read_start(client, function(err, chunk)
 			if chunk then
-				print("read data size: " .. string.len(chunk))
-				print(chunk)
+				log:debug("read data size: " .. string.len(chunk))
+				log:debug(chunk)
 				ret_res = chunk
 			else
-				print("nothing to read, close connection")
+				log:debug("nothing to read, close connection")
 				uv.close(client)
 			end
-		end)
-		print("connected to " .. u.host .. " now sending HTTP request")
+		end )
+		log:debug("connected to " .. u.host .. " now sending HTTP request")
 		req = "GET " .. u.path .. " HTTP/1.1\r\n" .. 
 			"Host: " .. u.host .. "\r\n" ..
 			"User-Agent: ezhttp\r\n" .. 
 			"Keep-Alive: 100\r\n" ..
 			"Connection: keep-alive\r\n" ..
 			"\r\n";
-        print("sending " .. req)
+        log:debug("sending " .. req)
 		uv.write(client, req)
-	end)
+	end )
 	uv.run('default')
 	uv.loop_close()
 	return ret_err, ret_res
 end
 
 _M.post = function(url, payload)
-	print("post")
+	
 end
 
-_M.getAsync = function(self)
-	print("getAsync")
+_M.getAsync = function(self, url)
+	table.insert(self.queue, {__status="pending", __stage="new"})
+	local myQuePos = #(self.queue)
+	local u = neturl.parse(url)
+	log:debug(u.host)
+	log:debug(u.scheme)
+	log:debug(u.port or u.services[u.scheme])
+
+	self.uv.getaddrinfo(u.host, nil, nil, function(err, addr)
+		if not err then
+			local client = self.uv.new_tcp()
+			uv.tcp_connect(client, addr, u.port or u.services[u.scheme], function(err) 
+				if not err then
+					uv.read_start(client, function(err, chunk) 
+						if err then
+							log.error(err)
+							self.queue[myQuePos] = {__status = "done", __stage = "on_read_start", data = nil, err = err}
+						else
+							if chunk then
+								self.queue[myQuePos].stage = "on_read_start"
+								self.queue[myQuePos].data = self.queue[myQuePos].data .. data
+							else
+								self.queue[myQuePos].__status = "done"
+								self.uv.close(client)
+					end ) -- read_start callback
+				else
+					log.error(err)
+					self.queue[myQuePos] = {__status = "done", __stage = "on_tcp_connect", data = nil, err = err}
+			end ) -- tcp_connect callback
+		else
+			log.error(err)
+			self.queue[myQuePos] = {__status = "done", __stage = "on_getaddrinfo", data = nil, err = err}
+	end ) -- getaddrinfo callback
 end
 
 _M.postAsync = function(self)
-	print("postAsync")
+	log:debug("postAsync")
 end
 
 _M.waitAll = function(self)
-	print("waitAll")
+	uv.run('default')
+	uv.loop_close()
+	return self.queue
 end
 
 _M.waitOne = function(self)
-	print("waitOne")
+	log:debug("waitOne")
 end
 
 return _M
---
+++ log.lua
@@ -0,0 +1,28 @@
+local _M = {}
+
+_M.priorityMap = {
+	debug = 0,
+	info = 1,
+	warn = 2,
+	error = 3,
+	fatal = 4
+}
+
+_M.new = function(self,level)
+	local t = {}
+	t.priority = self.priorityMap[level]
+	setmetatable(t, {__index = _M})
+	return t
+end
+
+_M.say = function(self, msg, level)
+	if self.priorityMap[level] >= self.priority then print("[" .. level .. "] " .. msg) end
+end
+
+for k, _ in pairs(_M.priorityMap) do
+	_M[k] = function(self, msg)
+		self:say(msg, k)
+	end
+end
+
+return _M
