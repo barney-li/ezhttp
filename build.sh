f43d6b9147f32c111d0de92123d970cbfaa3126a
barney <barney.li@outlook.com>
2017-06-04 08:15:09 -0700

    updated build framework

--- build.sh
+++ build.sh
@@ -1,1 +1,7 @@
+#(cd ./3rd/luv; make)
 cp ./3rd/luv/luv.so ./
+
+echo export LUA_PATH=$PWD'/?.lua;$LUA_PATH'>run_test.sh
+echo export LUA_CPATH=$PWD'/?.so;$LUA_CPATH'>>run_test.sh
+echo PATH=$PWD'/3rd/luv/build/:$PATH'>>run_test.sh
+echo 'luajit ./test/run_all.lua'>>run_test.sh
